f68fd229a9207448598357c0a47b601c8caf10db
shubham12pawargmailcom <shubham.12pawar@gmail.com>
2020-11-10 16:55:02 +0530

    commited first assign

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-const App = () => {
-  return (
-    <></>
-  )
+export default class App extends React.Component {
+ render (){
+   return (
+     <h1 data-ns-test="project-name">XYZ</h1>
+     <div data-ns-test="project-description"> good Project</div>
+     
+    
+   )
+ }
 }
 
 
-export default App;
