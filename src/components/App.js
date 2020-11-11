e5a47ad958ece6434dfd33624d5bd42f644be8cb
shubham12pawargmailcom <shubham.12pawar@gmail.com>
2020-11-11 12:16:07 +0530

    learned how to do in react

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,10 @@ import React, {Component,useState} from 'react';
 import '../styles/App.css';
 const App=() => {
     return(
+      <>
     <h3 data-ns-test="project-name">LearnReactApp </h3>
     <div data-ns-test="project-description">Good project</div>
+    </>
     )
 
 }
